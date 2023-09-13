fa2fccad0cb6acd64aa11f6c7304422d029e920e
Tudor <tudor.comparan@gmail.com>
2023-09-13 08:36:32 -0600

    prettier config  fix

--- prettier.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  plugins: [
-    require("prettier-plugin-tailwindcss"),
-    require("prettier-airbnb-config"),
-  ],
-  tailwindConfig: "./tailwind.config.js",
-  printWidth: 100,
-};
