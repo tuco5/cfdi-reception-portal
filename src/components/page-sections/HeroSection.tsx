5b38cc5b2ae51d2714f37dd48ce1a3c7ba9de673
Tudor Comparan <tudor.comparan@gmail.com>
2023-09-18 21:33:43 -0600

    Home page: Hero section (#10)
    * Hero section
    
    * export button as a reusable component

--- src/components/page-sections/HeroSection.tsx
+++ src/components/page-sections/HeroSection.tsx
@@ -1,10 +1,23 @@
-import Icon from '../Icon';
+import {twMerge} from 'tailwind-merge';
+import Button from '../Button';
 
 export default function HeroSection() {
   return (
-    <section className="flex min-h-screen items-center justify-center bg-slate-200">
-      Hero Section
-      <Icon name="friendly" className="h-6 w-6 text-red-500" />
+    <section className="flex min-h-screen flex-col items-center justify-center bg-hero bg-cover bg-fixed bg-center bg-no-repeat">
+      <div className="flex w-full max-w-[1400px] -translate-y-20 flex-col gap-14 text-white">
+        <h2 className="glow text-6xl drop-shadow">Administra tu empresa</h2>
+        <p className="text-4xl">
+          Crea un portal e invita a tus proveedores <br />
+          que carguen sus CFDI’s a tu portal... <br />
+          organiza tus pagos.
+        </p>
+        <div className="mt-16 flex w-full justify-center gap-28">
+          <Button className="w-full max-w-[190px]">Comienza Ya</Button>
+          <Button className="w-full max-w-[190px]" variant="second">
+            Planes...
+          </Button>
+        </div>
+      </div>
     </section>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,11 @@
-import type { Config } from 'tailwindcss';
+import type {Config} from 'tailwindcss';
 
 const config: Config = {
-  content: ['./src/pages/**/*.{js,ts,jsx,tsx,mdx}', './src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
   theme: {
     colors: {
       primary: '#EB11D7',
@@ -17,6 +21,7 @@ const config: Config = {
 
     extend: {
       backgroundImage: {
+        hero: "linear-gradient(to bottom right, rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.4)), url('/img/hero.jpg')",
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
