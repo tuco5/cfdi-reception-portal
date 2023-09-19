76546b2565e0da1bc72b66b1c8451db9c89c3fce
gsmarco <gsmarcoglez@gmail.com>
2023-09-18 18:44:26 -0600

    Se termino de codificar la sección AboutCard Terminada

--- src/components/page-sections/AboutSection.tsx
+++ src/components/page-sections/AboutSection.tsx
@@ -4,15 +4,19 @@ import AboutCard from './AboutCard';
 export default function AboutSection() {
   return (
     <section className="flex min-h-[90vh] flex-col items-center gap-10 bg-white">
-      <h2 className='h-16 mt-14 mb-8 text-5xl text-tertiary font-bold'>Fácil de usar....conoce más</h2>
+      <h2 className='h-16 mt-14 mb-8 text-5xl text-tertiary font-bold'>Fácil de usar, conoce más...</h2>
       <div className="flex items-center justify-center gap-14">
         <AboutCard title="Amigable" Icon={<Icon name="friendly"  className='w-24 h-24' />}>
           Plataforma fácil de usar. No requiere mayor capacitacion para comenzar.
         </AboutCard>
 
-        <AboutCard title="Estrucutrado" Icon={<Icon name='structure-mind' className='w-24 h-24' />}>
+        <AboutCard title="Estructurado" Icon={<Icon name='structure-mind' className='w-24 h-24' />}>
           Comienza hoy y organiza tus cuentas fácilmente con un portal. Invita a tus proveedores
           para que comiencen a cargar sus documentos fiscales.
+        </AboutCard>
+
+        <AboutCard title="Integral" Icon={<Icon name='bank' className='w-24 h-24' />}>
+        Integra aplicaciones de terceros, cómo tu banca favorita y programa pagos de forma automática.
         </AboutCard>
       </div>
     </section>
