7d37a57dfc7816b14c545bf53302e27138d75c4d
Tudor Comparan <tudor.comparan@gmail.com>
2023-10-17 16:56:58 -0600

    Feature: Signup Modal (#13)
    * configuration of nextui
    
    * nextui config tailwind
    
    * signup button with modal
    
    * revert deletion of footer

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,15 @@
+import {NextUIProvider} from "@nextui-org/react";
 import Footer from '@/components/Footer';
 import Navbar from '@/components/Navbar';
-import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <NextUIProvider>
       <Navbar />
       <Component {...pageProps} />
       <Footer />
-    </>
+    </NextUIProvider>
   );
 }
