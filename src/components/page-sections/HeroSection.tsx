f741ab783821a3aa1912d49197b931cf1c4935ef
Tudor Comparan <tudor.comparan@gmail.com>
2023-10-16 18:22:25 -0600

    update hero section

--- src/components/page-sections/HeroSection.tsx
+++ src/components/page-sections/HeroSection.tsx
@@ -4,8 +4,8 @@ import Button from '../Button';
 export default function HeroSection() {
   return (
     <section className="flex min-h-screen flex-col items-center justify-center bg-hero bg-cover bg-fixed bg-center bg-no-repeat">
-      <div className="flex w-full max-w-[1400px] -translate-y-20 flex-col gap-14 text-white">
-        <h2 className="glow text-6xl drop-shadow">Administra tu empresa</h2>
+      <div className="flex w-full max-w-[1400px] -translate-y-20 flex-col gap-14 px-14 text-white">
+        <h2 className="text-6xl drop-shadow">Administra tu empresa</h2>
         <p className="text-4xl">
           Crea un portal e invita a tus proveedores <br />
           que carguen sus CFDI’s a tu portal... <br />
