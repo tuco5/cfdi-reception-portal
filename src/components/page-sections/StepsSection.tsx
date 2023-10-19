4206eb4e3004ff79f90457a6cbd91ac119d8653b
Tudor Comparan <tudor.comparan@gmail.com>
2023-10-18 18:26:31 -0600

    Feature: Section Contact us (#18)
    * contact us card
    
    * finish the form
    
    * polish styles

--- src/components/page-sections/StepsSection.tsx
+++ src/components/page-sections/StepsSection.tsx
@@ -3,9 +3,9 @@ import Icon from '../Icon';
 
 export default function StepsSection() {
   return (
-    <section className="bg-slate-200 flex min-h-screen items-center justify-center bg-steps bg-cover bg-center bg-no-repeat">
-      <div className="flex h-screen w-full max-w-[1400px] flex-col items-center gap-40 pt-32">
-        <h2 className="text-center text-6xl font-semibold tracking-wider text-white drop-shadow">
+    <section className="flex min-h-screen items-center justify-center bg-steps bg-cover bg-center bg-no-repeat">
+      <div className="flex h-screen w-full max-w-[1400px] flex-col items-center justify-center gap-32">
+        <h2 className="text-center text-5xl font-semibold tracking-widest text-white drop-shadow">
           En simples pasos comienza ya...
         </h2>
 
@@ -32,7 +32,7 @@ export default function StepsSection() {
 
         <Button
           color="secondary"
-          className="w-fit px-16 text-lg hover:-translate-y-0.5"
+          className="w-fit px-14 text-xl hover:-translate-y-0.5"
           size="lg"
           radius="full"
         >
