a6baf0802a7dd82a847572b11926654e879c0e2b
Tudor Comparan <tudor.comparan@gmail.com>
2023-10-24 09:17:54 -0600

    initial dashboard (#23)

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,10 +1,36 @@
 import {inter} from '@/styles/fonts';
+import {Button} from '@nextui-org/react';
+import Link from 'next/link';
 import {twMerge} from 'tailwind-merge';
 
 export default function DashboardPage() {
+  const LinkStyle = 'px-4 py-2 hover:bg-slate-800 transition-all';
+
   return (
-    <main className={twMerge(inter.className, 'flex min-h-screen flex-col items-center')}>
-      Dashboard
+    <main className={twMerge(inter.className, 'flex flex-col items-center')}>
+      <div className="flex w-full">
+        <div className="flex h-screen w-full max-w-xs flex-col gap-2 bg-slate-500 py-4 text-white">
+          <h2 className="px-4 py-2 text-lg font-bold text-slate-800 ">Applicación</h2>
+          <Link href="/dashboard" className={LinkStyle}>
+            Mis Portales
+          </Link>
+          <Link href="#" className={LinkStyle}>
+            Usuarios
+          </Link>
+          <Link href="#" className={LinkStyle}>
+            Configuración
+          </Link>
+        </div>
+        <div className="flex w-full flex-col p-8">
+          <div className="flex gap-4">
+            <Link href="/portal/new">
+              <Button color="secondary" radius="sm" className="text-base">
+                Nuevo Portal
+              </Button>
+            </Link>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
