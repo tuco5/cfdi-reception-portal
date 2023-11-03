cec8f1bddc08c76cdf7cde5ff98cec599dbcd21b
Tudor Comparan <tudor.comparan@gmail.com>
2023-11-03 08:09:42 -0600

    upload logo form front end

--- src/components/forms/UploadLogoForm.tsx
+++ src/components/forms/UploadLogoForm.tsx
@@ -1,7 +1,16 @@
-export default function UploadLogoForm({onSubmit}: FormProps) {
+import Icon from '../Icon';
+
+export default function UploadLogoForm(props: Props) {
   return (
-    <form className="flex h-[675px] w-4/5 flex-col gap-6 rounded-2xl border-[3px] border-black bg-white p-8">
+    <div {...props}>
       <h2 className="text-4xl font-bold">Agrega tu logo</h2>
-    </form>
+
+      <label className="flex h-60 w-80 cursor-pointer flex-col items-center justify-center self-center rounded-lg border-2 border-dashed border-slate-500 bg-slate-200 text-sm transition-all hover:border-slate-700 hover:bg-slate-400">
+        <Icon name="upload" className="mb-4 h-20 w-20" />
+        <p>Arrastra o haz click para agregar una imagen</p>
+        <p>2MB tamaño máximo</p>
+        <input type="file" className="hidden" />
+      </label>
+    </div>
   );
 }
