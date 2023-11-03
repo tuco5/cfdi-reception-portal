9d6258476182d2a92bc9024ed74c900341652cf8
Marco Antonio González Solis <gsmarcoglez@gmail.com>
2023-11-03 08:11:02 -0600

    Revision de invitación a proveedores (#27)
    * Invitar a proovedores
    
    * TeamMembersForm
    
    ---------
    
    Co-authored-by: OmarDS242 <143144042+OmarDS242@users.noreply.github.com>

--- src/components/forms/InviteSuppliersForm.tsx
+++ src/components/forms/InviteSuppliersForm.tsx
@@ -1,7 +1,44 @@
+import {Button, Input} from '@nextui-org/react';
+
+
+
 export default function InviteSuppliersForm({onSubmit}: FormProps) {
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    onSubmit();
+    // save data on DB
+  };
+
+
   return (
-    <form className="flex h-[675px] w-4/5 flex-col gap-6 rounded-2xl border-[3px] border-black bg-white p-8">
-      <h2 className="text-4xl font-bold">Invita proveedores</h2>
+    <form
+      className="flex h-[675px] w-4/5 flex-col gap-6 rounded-2xl border-[3px] border-black bg-white p-8"
+      onSubmit={handleSubmit}
+    >
+      <h2 className="text-4xl font-bold">Agrega proveedores (opcional)</h2>
+      <div className="flex gap-6">
+        <Input
+          color="default"
+          label="Correo electrónico"
+          isRequired
+          isClearable
+          classNames={{inputWrapper: 'bg-slate-300'}}
+        />
+
+       
+      </div>
+      <div className="h-40"></div>
+      <Button
+        color="secondary"
+        variant="shadow"
+        radius="full"
+        className="mt-4 w-fit self-center px-8 text-xl"
+        size="lg"
+        type="submit"
+      >
+        Finalizar
+      </Button>
     </form>
   );
 }
